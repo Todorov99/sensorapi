9fa8dce2515d90416cd852339191e05b29977181
Todorov99 <toshko_m1999@abv.bg>
2022-05-02 22:15:27 +0300

    Update binaries

--- pkg/dto/mailsender.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type MailSenderDto struct {
-	Subject string   `json:"subject,omitempty"`
-	Cc      []string `json:"cc,omitempty"`
-	To      []string `json:"to,omitempty"`
-	Body    string   `json:"body,omitempty"`
-}
